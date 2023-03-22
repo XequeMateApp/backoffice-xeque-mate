f428021c7068897a401020732cdbd7bb1988b371
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-03-22 10:46:12 -0300

    Create delete-category.component.ts

---
+++ src/app/logged/categorys/components/delete-category/delete-category.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-delete-category',
+  templateUrl: './delete-category.component.html',
+  styleUrls: ['./delete-category.component.scss']
+})
+export class DeleteCategoryComponent implements OnInit {
+  productsData: any;
+  form: FormGroup;
+  constructor(
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+  ) {
+    this.form = this.formBuilder.group({
+      delete: [''],
+    })
+  }
+  ngOnInit(): void {
+    this.productsData = JSON.parse(localStorage.getItem('productsData'));
+  }
+  exit() {
+    this.modalService.dismissAll()
+  }
+  delete() {
+    window.alert('Delete  ')
+  }
+}
