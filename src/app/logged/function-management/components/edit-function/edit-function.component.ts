27fe4c076c861129bf09a4b2534e7fac56bf1fb0
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-03-22 00:47:13 -0300

    "It's not about the destination, it's about the journey."

--- src/app/logged/function-management/components/edit-function/edit-function.component.ts
+++ src/app/logged/function-management/components/edit-function/edit-function.component.ts
@@ -10,12 +10,8 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class EditFunctionComponent implements OnInit {
   form: FormGroup;
   productsData: any;
-  checkAdm: any;
-  checkProd: any;
-  checkKyc: any;
-  checkClient: any;
-  checkControlAccess: any;
-  checkNot: any;
+  editNotStatus: string;
+  checked: string;
   constructor(
     private modalService: NgbModal,
     private formBuilder: FormBuilder,
@@ -23,27 +19,14 @@ export class EditFunctionComponent implements OnInit {
     this.form = this.formBuilder.group({
       name: [''],
       status: [''],
-      adm: [''],
-      products: [''],
-      kyc: [''],
-      client: [''],
-      accessControl: [''],
-      notification: [''],
       funtions: [''],
     })
   }
   ngOnInit(): void {
     this.productsData = JSON.parse(localStorage.getItem('productsData'));
+    this.editNotStatus = this.productsData.status;
     this.form.controls['name'].setValue(this.productsData.name)
-    this.form.controls['name'].setValue(this.productsData.name)
-
-    this.checkAdm = this.form.controls['funcoes'].setValue(this.productsData.funcoes)
-    this.checkProd = this.form.controls['funcoes'].setValue(this.productsData.funcoes)
-    this.checkKyc = this.form.controls['funcoes'].setValue(this.productsData.funcoes)
-    this.checkClient = this.form.controls['funcoes'].setValue(this.productsData.funcoes)
-    this.checkControlAccess = this.form.controls['funcoes'].setValue(this.productsData.funcoes)
-    this.checkNot = this.form.controls['funcoes'].setValue(this.productsData.funcoes)
-
+    this.checked = this.productsData.funcoes;
   }
 
 
