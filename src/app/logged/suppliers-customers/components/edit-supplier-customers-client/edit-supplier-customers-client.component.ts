7b6e70dabc9e05f7e17ccd916374f8f32a211a6f
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-03-22 00:49:14 -0300

    Update edit-supplier-customers-client.component.ts

--- src/app/logged/suppliers-customers/components/edit-supplier-customers-client/edit-supplier-customers-client.component.ts
+++ src/app/logged/suppliers-customers/components/edit-supplier-customers-client/edit-supplier-customers-client.component.ts
@@ -10,7 +10,9 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class EditSupplierCustomersClientComponent implements OnInit {
   form: FormGroup;
   selectFile: any = [];
-  selectFileName: String;
+  selectFileName: string;
+  productsData: any;
+  FilesDoc: any;
   constructor(
     private modalService: NgbModal,
     private formBuilder: FormBuilder,
@@ -20,20 +22,33 @@ export class EditSupplierCustomersClientComponent implements OnInit {
       cnpjcpf: [''],
       contact: [''],
       email: [''],
+      supplier: [''],
       doc: [''],
 
     })
   }
   ngOnInit(): void {
+    this.productsData = JSON.parse(localStorage.getItem('productsData'));
 
+    this.FilesDoc = this.productsData.doc;
+    console.log(this.FilesDoc);
+    this.form.controls['name'].setValue(this.productsData.name);
+    this.form.controls['cnpjcpf'].setValue(this.productsData.cnpj);
+    this.form.controls['contact'].setValue(this.productsData.tel);
+    this.form.controls['email'].setValue(this.productsData.email);
+  }
+  downloadFile() {
+    const link = document.createElement('a');
+    link.href = 'data:application/pdf;base64,' + btoa(this.FilesDoc);
+    link.download = this.FilesDoc;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
   }
   exit() {
     this.modalService.dismissAll()
   }
   confirm() {
     window.alert('confirm ')
-  }
-  onSelectFileName(event) {    
-    this.selectFileName = event.target.files[0].name;
   }
 }
