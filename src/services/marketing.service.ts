a59eeaadb96d71a4794e966d8674fa7e5bd38d43
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-04-14 04:58:38 -0300

    Update marketing.service.ts

--- src/services/marketing.service.ts
+++ src/services/marketing.service.ts
@@ -4,8 +4,6 @@ import { Observable, Subject } from 'rxjs';
 import { catchError, map } from "rxjs/operators";
 import { environment } from 'src/environments/environment';
 import { BaseService } from './base.service';
-import { NotificationResponsetDto } from 'src/app/dto/logged/notification-response.dto';
-import { NotificationRegisterRequestDto } from 'src/app/dto/logged/notification-register-request.dto';
 import { MarketingResponseDto } from 'src/app/dto/logged/marketing-response.dto';
 import { MarketingRegisterRequestDto } from 'src/app/dto/logged/marketing-register-request.dto';
 
@@ -35,15 +33,17 @@ export class MarketingService extends BaseService {
       .pipe(map(this.extractData), catchError(this.serviceError));
   }
 
+  editMarketing(Id: string, dto: MarketingRegisterRequestDto): Observable<MarketingResponseDto> {
+    return this.httpClient
+      .put(`${this.url}update/id/${Id}`, dto, this.authorizedHeader)
+      .pipe(map(this.extractData), catchError(this.serviceError));
+  }
+
   deleteMarketing(Id: string): Observable<MarketingResponseDto> {
     return this.httpClient
       .delete(`${this.url}delete/id/${Id}`, this.authorizedHeader)
       .pipe(map(this.extractData), catchError(this.serviceError));
   }
 
-  editNotification(Id: string, dto: NotificationRegisterRequestDto): Observable<NotificationResponsetDto> {
-    return this.httpClient
-      .put(`${this.url}edit/${Id}`, dto, this.authorizedHeader)
-      .pipe(map(this.extractData), catchError(this.serviceError));
-  }
+
 }
