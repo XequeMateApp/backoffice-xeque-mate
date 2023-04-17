32f75a94b17cdf594abb9455061f289e7c77674e
luishdsg <65797541+luishdsg@users.noreply.github.com>
2023-04-17 16:11:51 -0300

    up

--- src/app/logged/dashboard/dashboard.component.ts
+++ src/app/logged/dashboard/dashboard.component.ts
@@ -32,7 +32,50 @@ export type ChartOptions = {
 
 export class DashboardComponent implements OnInit {
   @ViewChild("chart") chart: ChartComponent;
-  public productRequest: Partial<ChartOptions>;
+  public productRequest: Partial<ChartOptions> = {
+    series: [
+      {
+        name: "Produtos",
+        data: []
+      }
+    ],
+    chart: {
+      height: 350,
+      type: "line",
+      zoom: {
+        enabled: false
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: "straight"
+    },
+    title: {
+
+      align: "left"
+    },
+    grid: {
+      row: {
+        colors: ["#f3f3f3", "transparent"],
+        opacity: 0.5
+      }
+    },
+    xaxis: {
+      categories: [
+        "Seg",
+        "Ter",
+        "Qua",
+        "Qui",
+        "Sex",
+        "Sab",
+        "Dom",
+      ]
+    }
+  };
+  dataWeek: any[];
+;
   public newClientsRequest: Partial<ChartOptions>;
 
   dropDatas = '7 dias';
@@ -42,53 +85,13 @@ export class DashboardComponent implements OnInit {
   totalClients: string = '';
 
   daysFilter: number = 7
+  todos: any;
+  getWeekDaysName: any;
 
   constructor(
     private productService: ProductService,
     private userService: UserService
   ) {
-    this.productRequest = {
-      series: [
-        {
-          name: "Produtos",
-          data: [10, 41, 35, 51, 49, 62, 69]
-        }
-      ],
-      chart: {
-        height: 350,
-        type: "line",
-        zoom: {
-          enabled: false
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        curve: "straight"
-      },
-      title: {
-
-        align: "left"
-      },
-      grid: {
-        row: {
-          colors: ["#f3f3f3", "transparent"],
-          opacity: 0.5
-        }
-      },
-      xaxis: {
-        categories: [
-          "Seg",
-          "Ter",
-          "Qua",
-          "Qui",
-          "Sex",
-          "Sab",
-          "Dom",
-        ]
-      }
-    };
 
     this.newClientsRequest = {
       series: [
@@ -159,6 +162,34 @@ export class DashboardComponent implements OnInit {
   getProducts() {
     this.productService.getAllProducts().subscribe(
       success => {
+        console.log(success, 'fwfoewif')
+        this.getWeekDaysName = success.products.map(item => item.updatedAt);
+
+        const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        this.dataWeek = [];
+        weekdays.forEach(day => {
+          this.dataWeek.push(this.getWeekDaysName.filter(item => {
+            const datenew = new Date(item);
+            switch (day) {
+              case 'Monday':
+                return datenew.getDay() === 1;
+              case 'Tuesday':
+                return datenew.getDay() === 2;
+              case 'Wednesday':
+                return datenew.getDay() === 3;
+              case 'Thursday':
+                return datenew.getDay() === 4;
+              case 'Friday':
+                return datenew.getDay() === 5;
+              case 'Saturday':
+                return datenew.getDay() === 6;
+              case 'Sunday':
+                return datenew.getDay() === 0;
+              default: return false;
+            }
+          }).length)
+        })
+        this.productRequest.series[0].data === this.dataWeek;
         this.totalProducts = success.count.toString();
         console.log('Produtos Cadastrados', success.count);
       },
