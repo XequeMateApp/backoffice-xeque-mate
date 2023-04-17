be78f6df0bbea7ee2f809d194afdb421db629ffc
Danilo de Falco <57508216+danilodefalco@users.noreply.github.com>
2023-04-17 10:35:16 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,21 @@
 export const environment = {
   production: true,
  
+  firebaseConfig: {
+    apiKey: "AIzaSyD-t9J7dyLCFeMBW_VnfcAVVJYjRO2N9uY",
+    authDomain: "xeque-mate-2916e.firebaseapp.com",
+    projectId: "xeque-mate-2916e",
+    storageBucket: "xeque-mate-2916e.appspot.com",
+    messagingSenderId: "1071724999399",
+    appId: "1:1071724999399:web:884548bca3e3336621e842",
+    measurementId: "G-EPPEFW9HKT"
+  },
   maxImgageSize: 10485760,
   maxDocumentSize: 10485760,
-
   api: {
     xequeMateApi: 'https://xequemate.tgtdigital.io'
   },
+  encryptKey: '567e61fddbe8a715df61d805f2551b8a'
 
 };
 
